9de389d417f2db7eed638cbae441731bb195371f
Joe Watt <joewatt95@u.nus.edu>
2024-04-21 20:18:16 +0800

    tweak lemma

--- MyList.thy
+++ MyList.thy
@@ -73,6 +73,6 @@ qed
 corollary sum_eq_sum_tail_rec :
   "sum xs = sum_tailrec xs 0"
   \<comment> \<open>sledgehammer\<close>
-  by (metis acc_sum_eq_sum_tailrec_acc add_0)
+  by (metis acc_sum_eq_sum_tailrec_acc add_0_left)
 
 end
