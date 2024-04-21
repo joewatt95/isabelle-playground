f98eea9bfa6361503b7129fcf1ee0ab1f67734dc
Joe Watt <joewatt95@u.nus.edu>
2024-04-21 15:02:21 +0800

    wip

--- MyList.thy
+++ MyList.thy
@@ -4,7 +4,9 @@ imports Main
 
 begin
 
-datatype 'a list = Nil | Cons 'a "'a list"
+datatype 'a list
+  = Nil
+  | Cons 'a "'a list"
 
 fun append :: "'a list => 'a list => 'a list" where
   "append Nil ys = ys" |
@@ -16,44 +18,54 @@ fun reverse :: "'a list => 'a list" where
 
 value "reverse (Cons false (Cons true Nil))" 
 
-lemma append_nil [simp]:
-  "append xs Nil = xs" (is "?P xs")
-proof (induct xs)
-  show "?P Nil" by simp
-next
-  fix x xs
-  assume "?P xs"
-  then show "?P (Cons x xs)" by simp
+lemma append_nil [simp] :
+  "append xs Nil = xs"
+proof (induction xs)
+  case Nil show ?case by simp
+  case (Cons _ _) thus ?case by simp
 qed
 
-lemma append_cons [simp]:
-  "append xs (Cons y ys) = append xs (append (Cons y Nil) ys)" (is "?P xs")
-proof (induct xs)
-  show "?P Nil" by simp
-next
-  fix x xs
-  assume "?P xs"
-  then show "?P (Cons x xs)" by simp
+lemma append_cons [simp] :
+  "append xs (Cons y ys) = append xs (append (Cons y Nil) ys)"
+proof (induction xs)
+  case Nil show ?case by simp
+  case (Cons _ _) thus ?case by simp
 qed
 
-theorem reverse_reverse:
-  "reverse (reverse xs) = xs" (is "?P xs")
-proof (induct xs)
-  show "?P Nil" by simp
-next
-  fix x xs
-  assume "?P xs"
-  then show "?P (Cons x xs)" by simp
+theorem reverse_reverse :
+  "reverse (reverse xs) = xs"
+proof (induction xs)
+  case Nil show ?case by simp
+  case (Cons _ _) thus ?case by simp
 qed
 
-lemma reverse_append [simp]:
+lemma reverse_append [simp] :
   "reverse (append xs ys) = append (reverse ys) (reverse xs)" (is "?P xs")
-proof (induct xs)
-  show "?P Nil" by simp 
-next
-  fix x xs
-  assume "?P xs"
-  then show "?P (Cons x xs)" by simp
+proof (induction xs)
+  case Nil show ?case by simp 
+  case (Cons _ _) thus ?case by simp
+qed
+
+fun sum :: "nat list => nat" where
+  "sum Nil = 0" |
+  "sum (Cons x xs) = x + sum xs"
+
+fun sum_tailrec :: "nat list => nat => nat" where
+  "sum_tailrec Nil acc = acc" |
+  "sum_tailrec (Cons x xs) acc = sum_tailrec xs (x + acc)"
+
+lemma plus_sum_tailrec_eq_sum_tailrec_plus [simp] :
+  "sum_tailrec xs (x + acc) = x + sum_tailrec xs acc"
+proof (induction xs arbitrary: acc)
+  case Nil show ?case by simp
+  case (Cons _ _) thus ?case by smt
+qed
+
+theorem sum_eq_sum_tailrec :
+  "acc + sum xs = sum_tailrec xs acc"
+proof (induction xs)
+  case Nil show ?case by simp
+  case (Cons _ _) thus ?case by simp
 qed
 
 end
---
+++ MyNat.thy
@@ -0,0 +1,18 @@
+theory MyNat
+
+imports Main
+
+begin
+
+fun sum_up_to_of_fun :: "(nat => nat) => nat => nat" where
+  "sum_up_to_of_fun f 0 = f 0" |
+  "sum_up_to_of_fun f (Suc n) = f (n + 1) + (sum_up_to_of_fun f n)"
+
+theorem sum_formula :
+  "2 * sum_up_to_of_fun id n = n * (n + 1)"
+proof (induction n)
+  case 0 show ?case by simp
+  case (Suc _) thus ?case by simp
+qed
+
+end
