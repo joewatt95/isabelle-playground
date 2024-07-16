8d482a98932e0078aa5c9e02159c7985e32613f5
joewatt95 <joewatt95@u.nus.edu>
2024-07-16 12:47:51 +0800

    t

--- MyNat.thy
+++ MyNat.thy
@@ -13,7 +13,7 @@ theorem sum_formula :
   "2 * sumUpToOfFun id n = n * (n + 1)"
 proof (induction n)
   case 0 show ?case by simp
-  case (Suc _) thus ?case by simp
+  case (Suc _) then show ?case by simp
 qed
 
 lemma exists_bigger_nat_0 :
@@ -22,7 +22,7 @@ proof
   fix x :: nat
   let "?P y" = "y > x"
   have "?P (x + 1)" by simp
-  thus "\<exists> y. ?P y" ..
+  then show "\<exists> y. ?P y" ..
 qed
 
 lemma exists_bigger_nat_1 :
@@ -56,11 +56,11 @@ proof (induction n)
   {
     case 1
     assume "even' 0"
-    thus ?case by (simp add: even_odd.even_zero)
+    then show ?case by (simp add: even_odd.even_zero)
   next
     case 2
     assume "odd' 0"
-    thus ?case by simp
+    then show ?case by simp
   }
 next
   case (Suc n)
@@ -68,22 +68,22 @@ next
     case 1
     assume "even' (Suc n)"
 
-    thus ?case when "odd' n" (is ?thesis)
+    then show ?case when "odd' n" (is ?thesis)
       using Suc.IH(2) even_of_odd that by fastforce 
 
     obtain k where "n + 1 = 2 * k" by (metis "1" Suc_eq_plus1)
-    hence "n = 2 * (k - 1) + 1" by simp
-    thus ?thesis by blast
+    then have "n = 2 * (k - 1) + 1" by simp
+    then show ?thesis by blast
   next
     case 2
     assume "odd' (Suc n)"
 
-    thus ?case when "even' n" (is ?thesis)
+    then show ?case when "even' n" (is ?thesis)
       using Suc.IH(1) odd_of_even that by fastforce
 
     obtain k where "n + 1 = 2 * k + 1" by (metis "2" Suc_eq_plus1)
-    hence "n = 2 * k" by simp
-    thus ?thesis by blast
+    then have "n = 2 * k" by simp
+    then show ?thesis by blast
   }
 qed
 
@@ -93,17 +93,17 @@ lemma even'_odd'_of_even_odd :
     odd'_of_odd : "odd n \<Longrightarrow> odd' n"
 proof (induction rule: even_odd.inducts)
   case even_zero
-  thus ?case by simp
+  then show ?case by simp
 next
   case odd_of_even
   fix m assume "even m" and "even' m"
   then obtain k where "m = 2 * k" by blast
-  thus "odd' (m + 1)" by simp
+  then show "odd' (m + 1)" by simp
 next
   case even_of_odd
   fix m assume "odd m" and "odd' m"
   then obtain k where "m = 2 * k + 1" by blast
-  thus "even' (m + 1)"
+  then show "even' (m + 1)"
     by (metis ab_semigroup_add_class.add_ac(1) add.commute mult_2)
 qed
 
@@ -115,18 +115,18 @@ theorem odd_or_even :
   "odd n \<or> even n"
 proof (induction n)
   case 0
-  thus ?case using even_zero by fastforce
+  then show ?case using even_zero by fastforce
 next
   case (Suc n)
   assume "odd n \<or> even n"
 
-  thus ?case
+  then show ?case
   proof
     assume "odd n"
-    thus ?case using even_of_odd by fastforce
+    then show ?case using even_of_odd by fastforce
   next
     assume "even n"
-    thus ?case using odd_of_even by fastforce
+    then show ?case using odd_of_even by fastforce
   qed
 qed
 
@@ -154,8 +154,8 @@ next
   fix m n
   assume "leq m n" and "leq' m n"
   then obtain k where "n = m + k" by blast
-  hence "n + 1 = m + (k + 1)" by simp
-  thus "leq' m (n + 1)" by blast
+  then have "n + 1 = m + (k + 1)" by simp
+  then show "leq' m (n + 1)" by blast
 qed
 
 lemma leq_of_leq' :
@@ -164,16 +164,16 @@ proof (induction n arbitrary: m rule: less_induct)
   case (less n)
   then obtain k where n_eq_m_plus_k : "n = m + k" by blast
 
-  thus "leq m n"
+  then show "leq m n"
   proof (cases k)
     case 0
-    hence "n = m" by (simp add: n_eq_m_plus_k)
-    thus ?thesis using leq_self by blast
+    then have "n = m" by (simp add: n_eq_m_plus_k)
+    then show ?thesis using leq_self by blast
   next
     case (Suc k')
-    hence "n - 1 = m + k'" by (simp add: n_eq_m_plus_k)
-    hence "leq m (n - 1)" by (simp add: Suc less.IH n_eq_m_plus_k)
-    thus ?thesis using leq_succ_of_leq by blast
+    then have "n - 1 = m + k'" by (simp add: n_eq_m_plus_k)
+    then have "leq m (n - 1)" by (simp add: Suc less.IH n_eq_m_plus_k)
+    then show ?thesis using leq_succ_of_leq by blast
   qed
 qed
 
